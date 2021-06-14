f7733185804f0c3c050cd78378737206efefc938
sunshinembv <sunshinembv@gmail.com>
2021-06-14 15:07:32 +0300

    Singleton01

---
+++ src/Singleton/SingletonSync.java
@@ -0,0 +1,19 @@
+package Singleton;
+
+public class SingletonSync {
+    private static SingletonSync instance;
+
+    private SingletonSync() {
+    }
+
+    public static SingletonSync getInstance() {
+        if (instance == null) {
+            synchronized (SingletonSync.class) {
+                if (instance == null) {
+                    instance = new SingletonSync();
+                }
+            }
+        }
+        return instance;
+    }
+}
